0b874be928f983c27f026d9cbc3ce445ca29ecdb
Natalie Tagher <natalie.tagher@wework.com>
2018-02-10 00:23:26 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,24 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# puts "A Tic Tac Toe Board"
+# phrase = "A Tic Tac Toe Board"
+# puts first_line = ["    |    |    "]
+# puts second_line = [" ------------"]
+# puts third_line = ["    |    |    "]
+# puts fourth_line = [" ------------"]
+# puts fifth_line = ["    |    |    "]
+# def display_board
+#   puts phrase
+#   print first_line
+#   print second_line
+#   print third_line
+#   print fourth_line
+#   print fifth_line
+# end
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
