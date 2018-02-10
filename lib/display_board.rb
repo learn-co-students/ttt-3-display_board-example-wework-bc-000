b6d152da239cb559265bebccb260dc629666d8d9
Benjamin Carter <benjamin.carter@wework.com>
2018-02-10 21:21:53 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,3 +6,13 @@ def display_board
  puts "   |   |   "
 end
 
+def display_board
+  puts "A Tic Tac Toe Board"
+end
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
